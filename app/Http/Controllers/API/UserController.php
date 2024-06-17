9bd9167e4d56b00020db6acbc681e0a765ff9500
Hogunz <cjdc1414@gmail.com>
2024-06-17 11:41:50 +0800

    6/17/2024

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -43,7 +43,7 @@ class UserController extends Controller
         $user = Auth::user();
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()->json(['token' => $token], 200);
+        return response()->json(['token' => $token, 'user' => $user], 200);
     }
 
     public function user(Request $request)
